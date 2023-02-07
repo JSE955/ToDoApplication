68f80815a09cac8cff5610210dd7eec3da325eef
JEdwards <jordanscottedwards@gmail.com>
2023-02-07 14:57:57 -0500

    Implemented ability to add to To-Do list

--- src/main/java/com/edenterprises/springboot/todoapp/todo/TodoService.java
+++ src/main/java/com/edenterprises/springboot/todoapp/todo/TodoService.java
@@ -9,13 +9,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class TodoService {
 	private static List<Todo> todos = new ArrayList<>();
+	private static int todosCount = 0;
 	static {
-		todos.add(new Todo(1, "Jordan", "Learn AWS", LocalDate.now().plusYears(1), false));
-		todos.add(new Todo(2, "Jordan", "Learn DevOps", LocalDate.now().plusYears(2), false));
-		todos.add(new Todo(3, "Izzy", "Learn Full Stack Development", LocalDate.now().plusYears(1), false));
+		todos.add(new Todo(++todosCount, "Jordan", "Learn AWS", LocalDate.now().plusYears(1), false));
+		todos.add(new Todo(++todosCount, "Jordan", "Learn DevOps", LocalDate.now().plusYears(2), false));
+		todos.add(new Todo(++todosCount, "Izzy", "Learn Full Stack Development", LocalDate.now().plusYears(1), false));
 	}
 	
 	public List<Todo> findByUser(String username) {
 		return todos;
+	}
+	
+	public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
+		Todo todo = new Todo(++todosCount, username, description, targetDate, done);
+		todos.add(todo);
 	}
 }
