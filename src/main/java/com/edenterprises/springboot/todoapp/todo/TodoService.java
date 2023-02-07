c31f8787768583309f40804f42feb4801607661a
JEdwards <jordanscottedwards@gmail.com>
2023-02-06 20:46:52 -0500

    Create first version of To-do page

--- src/main/java/com/edenterprises/springboot/todoapp/todo/TodoService.java
+++ src/main/java/com/edenterprises/springboot/todoapp/todo/TodoService.java
@@ -1,5 +1,21 @@
 package com.edenterprises.springboot.todoapp.todo;
 
-public class TodoService {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+	private static List<Todo> todos = new ArrayList<>();
+	static {
+		todos.add(new Todo(1, "Jordan", "Learn AWS", LocalDate.now().plusYears(1), false));
+		todos.add(new Todo(2, "Jordan", "Learn DevOps", LocalDate.now().plusYears(2), false));
+		todos.add(new Todo(3, "Izzy", "Learn Full Stack Development", LocalDate.now().plusYears(1), false));
+	}
+	
+	public List<Todo> findByUser(String username) {
+		return todos;
+	}
 }
