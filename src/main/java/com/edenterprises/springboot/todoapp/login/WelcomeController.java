77a6c86d1cb16aa77fd859efa16989b49ca6f129
JEdwards <jordanscottedwards@gmail.com>
2023-02-08 20:44:38 -0500

    Refactoring hardcoding of user

--- src/main/java/com/edenterprises/springboot/todoapp/login/WelcomeController.java
+++ src/main/java/com/edenterprises/springboot/todoapp/login/WelcomeController.java
@@ -1,10 +1,11 @@
 package com.edenterprises.springboot.todoapp.login;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
@@ -13,7 +14,12 @@ public class WelcomeController {
 
 		@RequestMapping(value="/", method=RequestMethod.GET)
 		public String goToWelcomePage(ModelMap model) {
-			model.put("name", "Jordan");
+			model.put("name", getLoggedinUsername());
 			return "welcome";
+		}
+		
+		private String getLoggedinUsername() {
+			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+			return authentication.getName();
 		}
 }
