54fa184807c08183af6307bdf58eece05147eead
JEdwards <jordanscottedwards@gmail.com>
2023-02-08 20:51:30 -0500

    Implement multiple users

--- src/main/java/com/edenterprises/springboot/todoapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/edenterprises/springboot/todoapp/security/SpringSecurityConfiguration.java
@@ -16,14 +16,21 @@ public class SpringSecurityConfiguration {
 	@Bean
 	public InMemoryUserDetailsManager createUserDetailsManager() {
 		Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
+		UserDetails userDetails1 = createNewUser("Jordan", "dummy");
+		UserDetails userDetails2 = createNewUser("Izzy", "dummy");
+		
+		return new InMemoryUserDetailsManager(userDetails1, userDetails2);
+	}
+
+	private UserDetails createNewUser(String username, String password) {
+		Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
 		UserDetails userDetails = User.builder()
 									.passwordEncoder(passwordEncoder )
-									.username("theUser")
-									.password("dummy")
+									.username(username)
+									.password(password)
 									.roles("USER", "ADMIN")
 									.build();
-		
-		return new InMemoryUserDetailsManager(userDetails);
+		return userDetails;
 	}
 	
 	@Bean
