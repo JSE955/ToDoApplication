e911a90106526b53d4690ef2e848d26ecd9e6665
JEdwards <jordanscottedwards@gmail.com>
2023-02-08 21:22:06 -0500

    Loaded data into H2 database

--- src/main/java/com/edenterprises/springboot/todoapp/todo/Todo.java
+++ src/main/java/com/edenterprises/springboot/todoapp/todo/Todo.java
@@ -2,10 +2,19 @@ package com.edenterprises.springboot.todoapp.todo;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 
+@Entity
 public class Todo {
+	
+	@Id
+	@GeneratedValue
 	private int id;
+	
 	private String username;
 	
 	@Size(min=10, message="Enter at least 10 characters")
